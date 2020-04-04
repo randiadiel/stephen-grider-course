c6a065eac5f4fe20f90e046c0b662e144646d6ae
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-04-04 15:14:57 +0700

    Conditional Redering

--- src/index.js
+++ src/index.js
@@ -7,19 +7,27 @@ class App extends React.Component {
 
     // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT
     this.state = {
-      lat: null
+      lat: null,
+      errorMessage: ""
     };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
   // React says we have to define render!!
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading!</div>;
   }
 }
 
