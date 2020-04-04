8ed979450e7fbac69493cc5ed26f4ced540173c6
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-04-04 19:52:13 +0700

    Refactoring the SeasonDisplay

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT
-    this.state = {
-      lat: null,
-      errorMessage: ""
-    };
-
+  // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT
+  state = { lat: null, errorMessage: "" };
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
   // React says we have to define render!!
   render() {
+    const { state } = this;
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    } else if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <div>Error: {state.errorMessage}</div>;
+    } else if (!state.errorMessage && state.lat) {
+      return <SeasonDisplay lat={state.lat} />;
     }
     return <div>Loading!</div>;
   }
