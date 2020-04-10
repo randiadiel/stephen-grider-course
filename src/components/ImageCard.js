2fb860a70bace974b0b41b1f85c4dabc308c6673
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-04-10 15:25:12 +0700

    Customize the image dynamic rows

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,49 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  state = { spans: null };
+
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 1);
+    this.setState({ spans });
+  };
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  renderCard = () => {
+    const { spans } = this.state;
+    const { alt_description, urls } = this.props.image;
+    if (spans === null) {
+      return (
+        <div class="ui placeholder">
+          <div class="square image">
+            <img ref={this.imageRef} alt={alt_description} src={urls.regular} />
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+          <img ref={this.imageRef} alt={alt_description} src={urls.regular} />
+        </div>
+      );
+    }
+  };
+
   render() {
+    const { spans } = this.state;
     const { alt_description, urls } = this.props.image;
     return (
-      <div>
-        <img alt={alt_description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={alt_description} src={urls.regular} />
       </div>
     );
   }
